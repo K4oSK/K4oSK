566303b986a24a6e92ce2ce45defe3f9c5dfda24
K4 <96160989+K4oSK@users.noreply.github.com>
2021-12-14 22:33:17 -0600

    Create README.py

---
+++ README.py
@@ -0,0 +1,20 @@
+from GitHub import ReadMe
+
+class K4(ReadMe):
+
+    def __init__(self):
+        self.username = "K4"
+        self.contacts = {
+            "Discord": "k4#8888"
+        }
+        self.alises = {
+            "K4oS"
+            "K4"
+            "K4izen"
+            "iK4oS"
+             "K4oSK"
+        }
+        self.occupation = "None"
+        self.projects = {
+            "K4 Projects": "https://github.com/K4-Projects"
+        }
